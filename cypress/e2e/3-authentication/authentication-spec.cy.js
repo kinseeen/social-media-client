95125263318f7e4ac8eb769fed091cc2b27667a2
kinseeen <kine.jakobsen@hotmail.no>
2024-04-14 20:30:37 +0200

    remove cypress examples and add required e2e-tests

--- cypress/e2e/3-authentication/authentication-spec.cy.js
+++ cypress/e2e/3-authentication/authentication-spec.cy.js
@@ -2,13 +2,41 @@
 /// <reference types="Cypress"/>
 
 describe('Test authentication functionality', () => {
-  cy.beforeEach(() => {
-    cy.visit('/login');
+  beforeEach(() => {
+    cy.visit('/');
   });
 
-  it('should show a login form', () => {
-    cy.get('form').should('exist');
-    cy.get('input[name="email"]').should('exist');
-    cy.get('input[name="password"]').should('exist');
+  it('Should check that login works as expected', () => {
+    cy.validLogin()
+      .wait(500)
+      .then(() => {
+        // Check if token is stored
+        expect(localStorage.getItem('token')).to.exist;
+      });
+  });
+
+  it('should log out the user', () => {
+    cy.validLogin()
+      .wait(500)
+      .then(() => {
+        cy.logout()
+          .wait(500)
+          .then(() => {
+            // Check if token is removed
+            expect(localStorage.getItem('token')).to.not.exist;
+          });
+      });
+  });
+
+  it('Should check that login fails with wrong credentials, and gives an error message', () => {
+    const noroffAlert = cy.stub().as('noroffAlert');
+    cy.on('window:alert', noroffAlert);
+    cy.invalidLogin();
+
+    cy.get('@noroffAlert').should(
+      'have.been.calledOnceWith',
+      'Either your username was not found or your password is incorrect',
+    );
+    cy.get('@noroffAlert').should('have.been.calledOnce');
   });
 });
